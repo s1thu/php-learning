ff0ab1ac85afb052b1976738ed1882b21e050e8f
Si Thu Win <25-00205@dat.com>
2024-07-23 13:52:46 +0630

    form finished

--- public/form.php
+++ public/form.php
@@ -9,10 +9,40 @@
 </head>
 <body>
 <form action="">
-    <label for="number">Enter Number of Kitchen</label>
-    <input type="number" name="number" id="number" value="<?php
-    if (isset($_GET['number'])) {echo $_GET['number'];}
-    ?>">
+    <?php
+    $pictures =[
+        'cat.jpeg' => 'Cat',
+        'dog.jpeg' => 'Dog'
+    ]
+    ?>
+    <h1>Form</h1>
+    <label for="picture">
+        Select a picture:
+    </label>
+    <select name="picture" id="picture">
+        //code refactor using pictures array
+        <?php foreach($pictures as $picture => $name): ?>
+        <option value="<?=$picture?>"<?php
+        if (isset($_GET['picture'])
+            && $_GET['picture'] === $picture) {
+            ?> selected<?php
+        } ?>>
+        <?php //=ucfirst(explode('.', $picture)[0])
+        echo htmlspecialchars($name, ENT_QUOTES);
+        ?>
+        </option>
+        ?>
+        <?php endforeach; ?>
+    </select>
+    <div>
+        <label for="number">
+            Number of pictures to show:
+        </label>
+        <input name="number" value="<?php
+        if (isset($_GET['number'])) {
+            echo htmlspecialchars($_GET['number'], ENT_QUOTES); }
+        ?>">
+    </div>
     <button type="submit">Submit</button>
 </form>
 </body>
